6d4965495081a79f58886cbeb1feeefd25e7b0e8
Mokamal15 <Mokamal15falcon@gmail.com>
2023-10-09 18:50:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var myName = "FALCON BEEEEEEEEEEEEEH";
+
+window.addEventListener('DOMContentLoaded', function() {
+  var nameDiv = document.getElementById('name');
+  nameDiv.textContent = 'My name is: ' + myName;
+});
